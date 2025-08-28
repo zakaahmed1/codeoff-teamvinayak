94d5565b11fd637bd8c4e1954c002ed3ce2f6e8c
zakaahmed1 <zakaahmed1@hotmail.com>
2025-08-28 11:12:59 +0100

    Initial commit on branch question3

---
+++ question3.py
@@ -0,0 +1,5 @@
+import math
+
+# function to calculate the manhattan distance between two points
+def manhattan_distance(point1, point2):
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
